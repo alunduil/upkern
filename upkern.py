436f26659bed4f65bf80bdebbd140389a42181c4
Alex Brandt <alunduil@alunduil.com>
2010-02-01 00:07:53 +0000

    Reorganized the main script.  Now it's time to move on to others.

--- upkern.py
+++ upkern.py
@@ -20,177 +20,186 @@
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
 
-def main():
-    if os.getuid() != 0:
-        print "Superuser access is required!"
-        return 1;
+import time
+import sys
 
-    usage = "usage: %prog [options] kernel"
+class Upkern:
+    def __init__(self, argv):
+        self._debug = False
+        self._verbose = False
+        self._kernel_name = ""
+        self._configurator = ""
+        self._rebuild_modules = False
+        self._time_build = False
+        self._kernel_options = ""
+        self._editor = ""
 
-    parser = optparse.OptionParser(usage=usage)
+        usage = "usage: %prog [options] kernel"
 
-    configurators = [
-        "menuconfig",
-        "xconfig",
-        "gconfig",
-        "oldconfig",
-        "silentoldconfig",
-        "defconfig",
-        "allyesconfig",
-        "allmodconfig",
-        "allnoconfig",
-        "randconfig"
-        ]
+        parser = optparse.OptionParser(usage=usage)
 
-    configurator_help_list = [
-        "Specifies which configurator should be used to configure the kernel",
-        " sources. The configurator can be one of the following: ",
-        ", ".join(configurators),
-        " All the configurators are documented in the kernel source files."
-        ]
-    parser.add_option('--configurator', '-c', type='choice',
-        choices=configurators, default='menuconfig',
-        help=''.join(configurator_help_list))
+        try:
+            variables, arguments = self._parseOptions(argv, parser)
+        except:
+             raise UpkernArgumentException("", parser)
 
-    initrd_help_list = [
-        "Specifies the initial ramdisk file to the boot loader configuration."
-        ]
-    parser.add_option('--initrd', '-i', default='',
-        help=''.join(initrd_help_list))
+        self._debug = variables.debug
+        self._verbose = variables.verbose
 
-    options_help_list = [
-        "This string is literally tacked on to the kernel line in your boot",
-        " loader's configuration. This is the place you would want to stick",
-        " a framebuffer line, or any other options you want your kernel to",
-        " have."
-        ]
-    parser.add_option('--options', '-o', dest='kernel_options', default='',
-        help=''.join(options_help_list))
+        if variables.version: raise UpkernArgumentException("Upkern, version 3.0.0")
 
-    boot_splash_help_list = [
-        "Verifies the splash-utils are installed, and sets up the specified",
-        " boot splash theme to work on boot."
-        ]
-    parser.add_option('--boot-splash', '-b', dest='boot_splash', default='',
-        help=''.join(boot_splash_help_list))
+        if len(arguments) > 0: self._kernel_name = arguments[0]
 
-    sources_help_list = [
-        "Specify the sources that will by used for this kernel build. If not",
-        " specified, the gentoo sources will be used."
-        ]
-    parser.add_option('--sources', '-s', default='gentoo',
-        help=''.join(sources_help_list))
+        self._configurator = variables.configurator
+        self._rebuild_modules = variables.rebuild_modules
+        self._time_build = variables.time_build
+        self._kernel_options = variables.kernel_options
+        self._editor = variables.editor
 
-    editor_help_list = [
-        "Specify the editor to use for editing the boot loader configuration",
-        " file after " + sys.argv[0] + " has already modified it. The",
-        " default editor is the one defined by your ${EDITOR} environment",
-        " variable."
-        ]
-    parser.add_option('--editor', '-e', help=''.join(editor_help_list))
+    def Run(self):
+        try:
+            # Handle the kernel parts.
+            kernel = kernel.Kernel(self._configurator,
+                self._kernel_name, self._rebuild_modules, self._debug,
+                self._verbose)
+            kernel.Configure()
+            if self._time_build: start_time = time.time()
+            kernel.Build()
+            if self._time_build: stop_time = time.time()
+            kernel.Install()
+        except KernelException, error:
+            raise UpkernException(error.GetMessage())
 
-    verbose_help_list = [
-        "Sets the verbosity level, and how many messages are printed out as ",
-        sys.argv[0] + " runs. The levels are: none, error, warning, and",
-        " debug. To set the level specify multiple -v, or --verbose flags.",
-        " By setting the level, you are specifying the highest level",
-        " that will be printed to the standard output."
-        ]
-    parser.add_option('--verbose', '-v', action='count', dest='verbosity',
-        help=''.join(verbose_help_list))
+        try:
+            # Handle the boot loader stuffs.
+            boot_loader = bootloader.GetBootLoader(kernel,
+                self._kernel_options)
+            boot_loader.CreateConfiguration()
+            boot_loader.InstallConfiguration()
+        except BootLoaderException, error:
+            raise UpkernException(error.GetMessage())
 
-    rebuild_modules_help_list = [
-        "Makes " + sys.argv[0] + " use module-rebuild to rebuild the modules",
-        " you have pulled in via portage for the new kernel."
-        ]
-    parser.add_option('--rebuild-modules', '-r', action='store_true',
-        default=False, dest='rebuild_modules',
-        help=''.join(rebuild_modules_help_list))
+        if self._editor:
+            if len(self._editor) < 1:
+                self._editor = os.getenv("EDITOR", "")
+            output_string_list = [
+                self._editor + " " + boot_loader.GetConfigurationFile())
+                ]
+            os.system("".join(output_string_list))
 
-    time_help_list = [
-        "Times the actual build of the kernel allowing one to determine if",
-        " kernels are building faster based on different aspects of the",
-        " machine."
-        ]
-    parser.add_option('--time', '-t', action='store_true', dest='time_build',
-        default=False, help=''.join(time_help_list))
-
-    kernel_help_list = [
-        "Specifies a varying amount of information about the kernel you",
-        " would like to build. Ranges from kernel version to the full gentoo",
-        " specification."
-        ]
-    parser.add_option('--kernel', '-k', dest='kernel_name', default="",
-        help=''.join(kernel_help_list))
-
-    version_help_list = [
-        "Specifies the currently installed version of upkern."
-        ]
-    parser.add_option('--version', '-V', action='store_true', dest='version',
-        default=False, help=''.join(version_help_list))
-
-    options, arguments = parser.parse_args()
-
-    if len(arguments) != 0:
-        kernel_name = arguments[0]
-    else:
-        kernel_name = ""
-
-    if options.version:
-        print "upkern, version 2.0.11"
-        sys.exit(0)
-
-    try:
-        kernel = Kernel(options.configurator, options.kernel_name or \
-            kernel_name, options.sources, options.rebuild_modules, options.boot_splash)
-        kernel.configure(options.verbosity)
-        if (options.time_build): start_time = time.time()
-        kernel.build(options.verbosity)
-        if (options.time_build): stop_time = time.time()
-        kernel.install(options.verbosity)
-
-        boot_loader = create_bootloader(kernel, options.kernel_options,
-            options.initrd, options.boot_splash)
-        boot_loader.create_configuration()
-        boot_loader.install_configuration()
-
-        if options.editor:
-            if len(options.editor) <= 0:
-                options.editor = os.getenv("EDITOR", "")
-            os.system(options.editor + " " + boot_loader.config)
-
-        print "The kernel has been successfully upgraded to " + \
-            kernel.name + ".\n"
-        if (options.time_build):
-            print "The time to build the kernel was %(time).2f s\n" % \
-                {'time': stop_time - start_time}
-        output_list = [
-            "Please, check that all config files are in the appropriate",
-            " place, and that there are no errors in the configuration of",
-            " the boot process. It would be unfortunate if you were not able",
-            " to boot the new kernel we just prepared."
+        final_output_message_list = [
+            "The kernel has been successfully upgraded to ",
+            kernel.GetKernelName() + "."
+            ]
+        if self._time_build:
+            hours = int((stop_time - start_time)/3600)
+            minutes = int(((stop_time - start_time) % 3600)/60)
+            seconds = int((stop_time - start_time) % 60)
+            final_output_message_list[len(final_output_message_list):] = [
+                "The time to build the kernel was: " + hours + ":",
+                minutes + ":" + seconds "."
+                ]
+        final_output_message_list[len(final_output_message_list:] = [
+            "Please, check that all config files are in the ",
+            "appropriate place and that there are no errors in the ",
+            "configuration of the boot process.  It would be ",
+            "unfortunate if you were not able to boot the kernel we ",
+            "just prepared."
             ]
         for string in textwrap.wrap(''.join(output_list)):
             print string
-    except KernelException, error:
-        error.print_message()
-    except BootLoaderException, error:
-        error.print_message()
 
-    sys.exit(0)
+    def _parseOptions(self, argv, parser):
+        configurators = ["config", "menuconfig", "xconfig", "gconfig",
+            "oldconfig", "silentoldconfig", "defconfig",
+            "${PLATFORM}_defconfig", "allyesconfig", "allmodconfig",
+            "allnoconfig", "randconfig"
+            ]
+        configurator_help_list = [
+            "Specifies which configurator should be used to configure ",
+            "the kernel sources.  The configurator can be one of the ",
+            "following: " + ", ".join(configurators) + ".  All the ",
+            "configurators are documented in the kernel source files."
+            ]
+        parser.add_option('--configurator', '-c', type='choice',
+            choices=configurators, default='menuconfig',
+            help=''.join(configurator_help_list))
 
-class Upkern:
+        options_help_list = [
+            "This string is literally tacked onto the kernel options ",
+            "in your boot loader's configuration file.  This is the ",
+            "place you would want to stick a framebuffer line, or any ",
+            "other options you want your kernel to have."
+            ]
+        parser.add_option('--options', '-o', dest='kernel_options',
+            default='', help=''.join(options_help_list))
 
+        editor_help_list = [
+            "Specify the editor to use for editing the boot loader ",
+            "configuration file after " + argv[0] + " has already ",
+            "modified it.  The default editor is the one defined by ",
+            "your ${EDITOR} environment variable."
+            ]
+        parser.add_option('--editor', '-e',
+            help=''.join(editor_help_list))
+
+        verbose_help_list = [
+            "Sets verbose output."
+            ]
+        parser.add_option('--verbose', '-v', action='store_true',
+            default=False, dest='verbose',
+            help=''.join(verbose_help_list))
+
+        rebuild_modules_help_list = [
+            "Makes " + sys.argv[0] + " use module-rebuild to rebuild ",
+            "the modules you have pulled in via portage for the new ",
+            "kernel."
+            ]
+        parser.add_option('--rebuild-modules', '-r', action='store_true',
+            default=False, dest='rebuild_modules',
+            help=''.join(rebuild_modules_help_list))
+
+        time_help_list = [
+            "Times the actual build of the kernel allowing one to ",
+            "determine if kernels are building faster based on ",
+            "different aspects of the machine."
+            ]
+        parser.add_option('--time', '-t', action='store_true',
+            dest='time_build', default=False,
+            help=''.join(time_help_list))
+
+        kernel_help_list = [
+            "Specifies a varying amount of information about the ",
+            "kernel you would like to build.  Ranges from kernel ",
+            "version to the full gentoo specification."
+            ]
+        parser.add_option('--kernel', '-k', dest='kernel_name',
+            default="", help=''.join(kernel_help_list))
+
+        version_help_list = [
+            "Specifies the currently installed version of upkern."
+            ]
+        parser.add_option('--version', '-V', action='store_true',
+            dest='version', default=False,
+            help=''.join(version_help_list))
+
+        return parser.parse_args()
+
+class UpkernArgumentException:
+
+class UpkernException:
 
 if __name__ == '__main__':
     try:
         Upkern application(sys.argv)
         application->Run()
     except UpkernArgumentException, error:
-        if (len(error.GetMessage()) > 0): upkernoutput.error(error.GetMessage())
+        if (len(error.GetMessage()) > 0):
+            upkernoutput.error(error.GetMessage())
         print error.GetDescription()
         return 1
     except UpkernException, error:
-        if (len(error.GetMessage()) > 0): upkernoutput.error(error.GetMessage())
+        if (len(error.GetMessage()) > 0):
+            upkernoutput.error(error.GetMessage())
         return 1
     return 0
