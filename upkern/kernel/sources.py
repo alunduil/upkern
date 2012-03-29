2f41ec4d007833a66e61d05cb898d1d5bba8be1c
Alex Brandt <alunduil@alunduil.com>
2012-03-28 21:03:54 -0500

    Have refactored the last few properties.
    Split out package_name and directory_name into properties that are
    independent enough to work.

--- upkern/kernel/sources.py
+++ upkern/kernel/sources.py
@@ -49,6 +49,7 @@ class Sources(object):
         """
 
         self.arguments = {
+                "name": name,
                 "debug": debug,
                 "verbose": verbose,
                 "quiet": quiet,
@@ -64,44 +65,65 @@ class Sources(object):
 
     @property
     def directory_name(self):
-        pass # self._get_kernel_names(kernel_name)
+        """Get the sources directory."""
+        if not hasattr(self, "_directory_name"):
+            finder = FileOwner()
+
+            for directory in self.source_directories:
+                if unicode(finder((directory, ))[0][0]) == self.package_name.lstrip("="):
+                    self._directory_name = directory
+                    break
+
+        return self._directory_name
 
     @property
     def package_name(self):
-        pass # self._get_kernel_names(kernel_name)
+        """Get the package name of the kernel we're dealing with."""
+        if not hasattr(self, "_package_name"):
+            package_expression_list = [
+                    r"(?:sys-kernel/)?", # For piping from portage ...
+                    r"(?:(?P<sources>[+\w][+-\w]*)-sources-)?",
+                    r"(?P<version>[+\w][+-.\w]*)",
+                    ]
+            package_expression = re.compile("".join(emerge_expression_list))
+            package_match = package_expression.match(self.arguments["name"])
+
+            sources = "gentoo"
+
+            self._package_name = "=sys-kernel/"
+
+            if package_match:
+                if package_match.group("sources"):
+                    sources = package_match.group("sources")
+                self._package_name += sources
+                self._package_name += "-sources-"
+                self._package_name += package_match.group("version")
+            else:
+                finder = FileOwner()
+                self._package_name = unicode(finder((self.source_directories[0], ))[0][0])
+
+        return self._package_name
 
     @property
-    def install_image(self):
-        """Returns the name of the install image for this architecture.
-        
-        Return bzImage, vmlinux, etc depending on the arch of the machine.
+    def source_directories(self):
+        """Get the list of sorted source directories on the system."""
 
-        From the kernel README:
-          
-        Although originally developed first for 32-bit x86-based PCs (386 or
-        higher), today Linux also runs on (at least) the Compaq Alpha AXP, Sun
-        SPARC and UltraSPARC, Motorola 68000, PowerPC, PowerPC64, ARM, Hitachi
-        SuperH, Cell, IBM S/390, MIPS, HP PA-RISC, Intel IA-64, DEC VAX, AMD
-        x86-64, AXIS CRIS, Xtensa, AVR32 and Renesas M32R architectures.
+        if not hasattr(self, "_source_directories"):
+            self._directories = [ d for d in os.listdir('/usr/src') if re.match(r"linux-.+$", d) ]
 
-        We will support more of these as we get accurate reports of what image
-        is produced by the default make command.
+            keys = [ self._keyify(d) for d in directories ]
+            dict_ = dict(zip(keys, directories))
 
-        """
+            self._directories = [ dict_[d] for d in sorted(dict_.keys(), reverse = True) ]
 
-        if not hasattr(self, "_install_image"):
-            self._install_image = {
-                    "x86_64": "bzImage",
-                    "i686": "bzImage",
-                    }[platform.machine()]
-
-        return self._install_image
+        return self._directories
 
     @property
     def suffix(self):
         pass # "-" + self._directory_name.partition('-')[2]
 
     def prepare(self, configuration = ""):
+        self._install_sources()
         self._set_symlink()
         self._copy_config(configuration)
 
@@ -122,63 +144,38 @@ class Sources(object):
         if self.arguments["verbose"]:
             helpers.verbose("Rebuilding Modules: True")
 
-    @mountedboot
-    def _copy_config(self, configuration = ""):
-        """Copy the configuration file into /usr/src/linux/.config.
-        
+    def _install_sources(self):
+        """Installs the requested kernel sources using portage.
+
         This operation can be assumed to be atomic.  If a failure occurs all
-        actions taken up to that point shall be reverted and an appropriate 
+        actions taken up to that point shall be reverted and an appropriate
         exception raised.
-        
+
         """
 
-        # If no configuration file is passed; find the highest versioned one
-        # in /boot.
-        if not len(configuration):
-            config_list = [ 
-                    f for f in os.listdir('/boot') \
-                            if re.match('config-.+', f) 
+        if not len(GentoolkitQuery(self.package_name).find_installed()):
+            opts = [
+                    "-n",
+                    "-1",
                     ]
 
-            keys = map(self._create_kernel_key, config_list)
-            config_dict = dict(zip(keys, config_list))
+            if self.arguments["verbose"]:
+                opts.append("-v")
 
-            config_list = map(lambda x: config_dict[x], 
-                    sorted(config_dict.keys()))
+            if self.arguments["quiet"]:
+                opts.append("-q")
 
-            if len(config_list):
-                configuration = config_list[-1]
-
-        # If we didn't find a configuration file there is nothing further to
-        # do and we can return.
-        if not len(configuration):
-            return
-
-        if self.arguments["verbose"]:
-            helpers.verbose("Using Configuration File: %s", configuration)
-
-        # Perform the necessary actions (outlined in dry_run performed 
-        # otherwise).  Keeping in mind that any action on the system itself
-        # must be undone if an error occurs.
-        if self.arguments["dry_run"]:
-            dry_list = [
-                    "cp /usr/src/linux/.config{,.bak}",
-                    "cp /boot/%s /usr/src/linux/.config",
-                    ]
-            helpers.colorize("GREEN", 
-                    "\n".join(dry_list).format(configuration))
-        else:
-            try:
-                shutil.copy('/usr/src/linux/.config', 
-                        '/usr/src/linux/.config.bak')
-                shutil.copy('/boot/%s'.format(configuration), 
-                        '/usr/src/linux/.config')
-            except Exception e:
-                if os.access("/usr/src/linux/.config.bak", os.W_OK):
-                    os.remove("/usr/src/linux/.config")
-                    os.rename("/usr/src/linux/.config.bak", 
-                            "/usr/src/linux/.config")
-                raise e
+            command = "emerge %s %s".format(" ".join(opts),
+                    self.package_name)
+            
+            if self.arguments["dry_run"]:
+                helpers.colorize("GREEN", command)
+            else:
+                if helpers.sufficient_privileges():
+                    # TODO Can this fail in a way we need to revert?
+                    status = subprocess.call(command, shell = True)
+                    if status != 0:
+                        pass # Raise an appropriate exception.
 
     def _set_symlink(self):
         """Sets the symlink to the new kernel directory.
@@ -218,121 +215,64 @@ class Sources(object):
                 os.symlink(original, "/usr/src/linux")
                 raise e
 
-    def _get_kernel_names(self, kernel_name):
-        """Gets the names for this kernel.
-
-        Returns the qualified package name (emerge's name) as well as
-        the directory for the sources.
-
-        """
-        emerge_expression_list = [
-            '(?:sys-kernel/)?', # Just in case people want to pipe it
-                                # in from portage.
-            '(?:(?P<sources>[A-Za-z0-9+_][A-Za-z0-9+_-]*)-sources-)?',
-            '(?P<version>[A-Za-z0-9+_][A-Za-z0-9+_.-]*)'
-            ]
-        emerge_expression = re.compile("".join(emerge_expression_list))
-        emerge_match = emerge_expression.match(kernel_name)
-
-        if self._debug and emerge_match:
-            output.debug(__file__, {
-                'emerge_match':emerge_match.group(0)
-                })
-
-        sources = "gentoo"
-
-        emerge_name = "sys-kernel/"
-        directory_name = ""
-        if emerge_match:
-            
-            if emerge_match.group("sources"):
-                sources = emerge_match.group("sources")
-            emerge_name += sources
-            emerge_name += "-sources-"
-            emerge_name += emerge_match.group("version")
-            if self._debug: 
-                output.debug(__file__, {"emerge_name":emerge_name})
-            
-            # Get the directory name now.
-            from gentoolkit.query import Query
-            installed = Query(emerge_name).find_installed()
-            if self._debug:
-                output.debug(__file__, {
-                    "installed":installed
-                    })
-            if len(installed) < 1: 
-                # @todo Make this more efficient.
-                # Install the package requested.
-                #from portage import merge
-                #from gentoolkit import split_cpv
-                #category, pkg_name, version, revision = \
-                #    split_cpv(emerge_name)
-                #merge(category, pkg_name+version+revision
-                # @note The following is ugly!
-                opts = []
-                if self._verbose: opts.append("-v")
-                if self._quiet: opts.append("-q")
-                command_list = ["emerge", " ".join(opts), "="+emerge_name]
-                # @todo Any better way to do this?
-                if not self._dry_run and os.getuid() != 0:
-                    raise KernelException("Insufficient priveleges to continue!")
-                if self._dry_run:
-                    output.verbose(" ".join(command_list))
-                    raise KernelException("Cannot continue dry run without sources")
-                else:
-                    os.system(" ".join(command_list))
-
-            directories = self._get_kernel_directories()
-            directories.reverse()
-            from gentoolkit.helpers import FileOwner
-            finder = FileOwner()
-            for directory in directories:
-                if self._debug:
-                    output.debug(__file__, {
-                        "directory":directory,
-                        "finder(directory)":finder((directory,))[0][0],
-                        "emerge_name":emerge_name
-                        })
-                if unicode(finder((directory,))[0][0]) == emerge_name.strip():
-                    directory_name = directory
-                    break
-        else:
-            directory_name = self._get_kernel_directories()[-1]
-            from gentoolkit.helpers import FileOwner
-            finder = FileOwner()
-            emerge_name = finder((directory_name,))[0][0]
-
-        if self._debug: 
-            output.debug(__file__, {"directory_name":directory_name})
-            output.debug(__file__, {"emerge_name":emerge_name})
-        return (directory_name, emerge_name)
-
-    def _get_kernel_directories(self):
-        """Get the sorted and filtered list of kernel directories
-
-        """
-        if not os.access('/usr/src/', os.F_OK):
-            raise KernelException("Could not access /usr/src/")
-        src_list = os.listdir('/usr/src')
-        src_list = filter(lambda x: re.match('linux-.+$', x), src_list)
-
-        keys = map(self._create_kernel_key, src_list)
-        kernel_dict = dict(zip(keys, src_list))
-
-        if self._debug: output.debug(__file__, {"keys": keys, "src_list": src_list, "kernel_dict": kernel_dict})
-
-        result_list = map(lambda x: kernel_dict[x], sorted(kernel_dict.keys()))
-        if self._debug: output.debug(__file__, {"result_list": result_list})
+    @mountedboot
+    def _copy_config(self, configuration = ""):
+        """Copy the configuration file into /usr/src/linux/.config.
         
-        return result_list
-
-    def _create_kernel_key(self, kernel_string):
-        """Convert a kernel string into majorminorpatch.revision notation
+        This operation can be assumed to be atomic.  If a failure occurs all
+        actions taken up to that point shall be reverted and an appropriate 
+        exception raised.
+        
         """
-        if self._debug: output.debug(__file__, {"kernel_string": kernel_string})
+
+        # If no configuration file is passed; find the highest versioned one
+        # in /boot.
+        if not len(configuration):
+            config_list = [ f for f in os.listdir('/boot') if re.match('config-.+', f) ]
+
+            keys = [ self._keyify(c) for c in config_list ]
+            dict_ = dict(zip(keys, config_list))
+
+            config_files = [ dict_[k] for k in sorted(dict_.keys()) ] 
+
+            if len(config_list):
+                configuration = config_list[-1]
+
+        # If we didn't find a configuration file there is nothing further to
+        # do and we can return.
+        if not len(configuration):
+            return
+
+        if self.arguments["verbose"]:
+            helpers.verbose("Using Configuration File: %s", configuration)
+
+        # Perform the necessary actions (outlined in dry_run performed 
+        # otherwise).  Keeping in mind that any action on the system itself
+        # must be undone if an error occurs.
+        if self.arguments["dry_run"]:
+            dry_list = [
+                    "cp /usr/src/linux/.config{,.bak}",
+                    "cp /boot/%s /usr/src/linux/.config",
+                    ]
+            helpers.colorize("GREEN", 
+                    "\n".join(dry_list).format(configuration))
+        else:
+            try:
+                shutil.copy('/usr/src/linux/.config', 
+                        '/usr/src/linux/.config.bak')
+                shutil.copy('/boot/%s'.format(configuration), 
+                        '/usr/src/linux/.config')
+            except Exception e:
+                if os.access("/usr/src/linux/.config.bak", os.W_OK):
+                    os.remove("/usr/src/linux/.config")
+                    os.rename("/usr/src/linux/.config.bak", 
+                            "/usr/src/linux/.config")
+                raise e
+
+    def _keyify(self, kernel_string):
+        """Convert a kernel string into majorminorpatch.revision notation."""
         
         regex = '.*?(?P<major>\d+)\.(?P<minor>\d+)(?:\.(?P<patch>\d+))?-[^-]*(?:-r(?P<revision>\d+))?'
-        if self._debug: output.debug(__file__, {"regex": regex})
         m = re.match(regex, kernel_string)
         
         revision = major = minor = patch = 0
@@ -346,10 +286,7 @@ class Sources(object):
             if m.group("patch"):
                 patch = int(m.group("patch"))
 
-        if self._debug: output.debug(__file__, {"revision": revision})
-       
-        key = "%03d%03d%03d.%03d" % (major, minor, patch, revision)
-        if self._debug: output.debug(__file__, {"key": key})
+        key = "%03d%03d%03d.%03d".format(major, minor, patch, revision)
         
         return key
             
