ce4de85ffc560f5de8ade60be91a80257c8b9a82
Alex Brandt <alunduil@alunduil.com>
2012-03-29 20:19:14 -0500

    Added missing dependency on mountedboot.

--- upkern/kernel/sources.py
+++ upkern/kernel/sources.py
@@ -16,6 +16,8 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place - Suite 330, Boston, MA  02111-1307, USA.            
 
+from upkern.helpers import mountedboot
+
 class Sources(object):
     """A kernel source model.
 
