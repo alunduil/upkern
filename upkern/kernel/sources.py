bdceae00fc5566b8243370c29a47c9ada7271d70
Alex Brandt <alunduil@alunduil.com>
2012-03-29 20:32:54 -0500

    Adding an import of upkern.helpers.

--- upkern/kernel/sources.py
+++ upkern/kernel/sources.py
@@ -17,6 +17,7 @@
 # Place - Suite 330, Boston, MA  02111-1307, USA.            
 
 import re
+import upkern.helpers as helpers
 
 from upkern.helpers import mountedboot
 from gentoolkit import query as GentoolkitQuery
