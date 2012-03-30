70d0be1c7ec68cf20a42c4d4096647e81f510d15
Alex Brandt <alunduil@alunduil.com>
2012-03-29 20:20:23 -0500

    Adding import of gentoolkit.query to sources.

--- upkern/kernel/sources.py
+++ upkern/kernel/sources.py
@@ -17,6 +17,7 @@
 # Place - Suite 330, Boston, MA  02111-1307, USA.            
 
 from upkern.helpers import mountedboot
+from gentoolkit import query as GentoolkitQuery
 
 class Sources(object):
     """A kernel source model.
