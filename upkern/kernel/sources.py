f0d465d22b2324897e90672f392dc229abf97b80
Alex Brandt <alunduil@alunduil.com>
2012-03-29 20:21:04 -0500

    Adding import of re to sources.

--- upkern/kernel/sources.py
+++ upkern/kernel/sources.py
@@ -16,6 +16,8 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place - Suite 330, Boston, MA  02111-1307, USA.            
 
+import re
+
 from upkern.helpers import mountedboot
 from gentoolkit import query as GentoolkitQuery
 
