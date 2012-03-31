53512c34fdc8aed902b32f0c6e91eb63763b4789
Alex Brandt <alunduil@alunduil.com>
2012-03-30 22:07:09 -0500

    Adding the basebootloader to grub.

--- upkern/bootloader/bootloaders/grub.py
+++ upkern/bootloader/bootloaders/grub.py
@@ -16,6 +16,8 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place - Suite 330, Boston, MA  02111-1307, USA.             
 
+from upkern.bootloader.base import BaseLoader
+
 class Grub(BaseBootLoader):
     """A specific boot loader, GRUB, handler.
 
