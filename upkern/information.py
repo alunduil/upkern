af43a280ed97ed0a04c87941a72427b4777159a8
Alex Brandt <alunduil@alunduil.com>
2013-12-28 21:22:23 -0600

    Add information module to upkern.

---
+++ upkern/information.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2013 by Alex Brandt <alunduil@alunduil.com>
+#
+# upkern is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+NAME = 'upkern'
+VERSION = '9999'
+DESCRIPTION = \
+        'Gentoo kernel updater.'
+AUTHOR = 'Alex Brandt'
+AUTHOR_EMAIL = 'alunduil@alunduil.com'
+URL = 'https://github.com/alunduil/upkern'
+LICENSE = 'MIT'
+
+COPY_YEAR = '2014'
